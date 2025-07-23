7ec02490f0261a8ed1128c2f17f25e5d6b584042
Juan-SEO <jalgazam@gmail.com>
2025-07-23 21:05:10 +0200

    Cmabio infra

---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+bucket_name = "juan-terraform-iac-demo"
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Nombre del bucket S3"
+  type        = string
+}
