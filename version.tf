616b1dfc7107e2764305803fae92beed4ebfedc1
Juan-SEO <jalgazam@gmail.com>
2025-07-24 08:21:29 +0200

    Fix errors

--- version.tf
+++ version.tf
@@ -9,7 +9,7 @@ terraform {
     backend "s3" {
         bucket = "juan-terraform-backend"
         key    = "states/dev.tfstate"
-        region = "ap-southeast-2"
+        region = "us-east-1"
 
     }
     
